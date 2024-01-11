0c52ab63b6bb71e3aec45cb82e54275e2d7e4ebb
Jordi Homedes <jordihomedes48@gmail.com>
2024-01-11 13:45:25 +0100

    Service Discovery Implemented

--- order-service/src/main/java/com/jordiproject/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/jordiproject/orderservice/service/OrderService.java
@@ -21,7 +21,7 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
-    private final WebClient webClient;
+    private final WebClient.Builder webClientBuilder;
     public void placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
@@ -36,8 +36,8 @@ public class OrderService {
         List<String> skuCodes = orderLineItems.stream().map(OrderLineItem::getSkuCode).toList();
 
         //We call inventory Service to see if products are on stock (we use Synchronous communication)
-        InventoryResponse[] inventoryResponsesArray = webClient.get()
-                        .uri("http://localhost:8082/api/inventory",
+        InventoryResponse[] inventoryResponsesArray = webClientBuilder.build().get()
+                        .uri("http://inventory-service/api/inventory",
                                 uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
                         .retrieve()
                         .bodyToMono(InventoryResponse[].class)
